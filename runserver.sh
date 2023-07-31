eda084b09e74d711c2bdb1387d08309298758b16
JunYoung_Kim <hjklo0220@gmail.com>
2023-07-31 15:49:19 +0900

    update runserver.sh

--- runserver.sh
+++ runserver.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-APP_NAME=devops6th
-
 # git pull
 echo "git pull"
 git pull
@@ -11,5 +9,5 @@ echo "venv activate"
 source venv/bin/activate
 
 # runserver
-python3 $APP_NAME/manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver 0.0.0.0:8000
 
