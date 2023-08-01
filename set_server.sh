91afbac200bf6136d345ffa6735b326cc81ea199
JunYoung_Kim <hjklo0220@gmail.com>
2023-08-01 13:07:56 +0900

    set_server 이스케이프 추가

--- set_server.sh
+++ set_server.sh
@@ -36,12 +36,12 @@ echo "create niginx config"
 sudo sh -c "cat > /etc/nginx/sites-available/django <<EOF
 server {
 	listen 80;
-	server_name \$SERVER_IP;
+	server_name $SERVER_IP;
 	
 	location / {
 		proxy_pass http://127.0.0.1:8000;
-		proxy_set_header Host \$host;
-		proxy_set_header X-Real-IP \$remote_addr;
+		proxy_set_header Host \\\$host;
+		proxy_set_header X-Real-IP \\\$remote_addr;
 	}
 }
 EOF"
