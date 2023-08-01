e14df9f51ccd84e34a72e5ac348b402421e37e37
JunYoung_Kim <hjklo0220@gmail.com>
2023-08-01 11:59:46 +0900

    run_gunicorn 수정

--- run_gunicorn.sh
+++ run_gunicorn.sh
@@ -3,5 +3,8 @@
 # 실행 경로 진입
 cd /home/lion/devops6th
 
+# activatevenv
+source /home/lion/devops6th/venv/bin/activate
+
 #gunicorn 실행
 gunicorn lion_app.wsgi:application --config lion_app/gunicorn_config.py 
