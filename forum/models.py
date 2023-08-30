23b8499073eb31592d02f31af5cf7396759b9457
JunYoung_Kim <hjklo0220@gmail.com>
2023-08-30 10:32:48 +0900

    fix: image_url 모델 필드 추가

--- forum/models.py
+++ forum/models.py
@@ -30,6 +30,7 @@ class Post(models.Model):
     title = models.TextField(max_length=200)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
+    image_url = models.URLField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
