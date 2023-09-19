ede4debdbd0cb05f4e4fb67db75aa26146205e9c
JunYoung_Kim <hjklo0220@gmail.com>
2023-09-19 09:34:18 +0900

    [feat] add version check

--- common/middleware.py
+++ common/middleware.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from django.conf import settings
 
 
 class HealthCheckMiddleware:
@@ -6,6 +7,8 @@ class HealthCheckMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        if settings.VERSION == "0.3.0":
+            return JsonResponse({"status": "unhealthy"}, status=500)
         if request.path == "/health/":
             return JsonResponse({"status": "OK"})
 
